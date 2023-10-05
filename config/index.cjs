b4feb8de4c7f9ef070a4264a147eb21db282f47c
ancfeng <118799372+ancfeng@users.noreply.github.com>
2023-10-05 23:30:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'SuperMan',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'gh_69b08c4a0385',
+      id: 'oAXxy6c52KkEERnILD5XLvVwZrjI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'jsPo5pmKPvx65sxbizQPxshTZoUn0dPjDNhoo-riedU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
