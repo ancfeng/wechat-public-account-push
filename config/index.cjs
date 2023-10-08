b8e5cf98612f78961aae3097f9d0079f4d0490ec
ancfeng <118799372+ancfeng@users.noreply.github.com>
2023-10-08 08:06:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oAXxy6c52KkEERnILD5XLvVwZrjI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'PrBRCi37811uMl03NSmMcYFa0trD_LqKWLtN6UtOiXc',
+      useTemplateId: 'S2wBOXug9QVfGvpPshWPbgI4obqRC8dFZ42zo524QCA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-26',
       festivals: [
